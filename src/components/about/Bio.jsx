93c95182c5ac247c5d509139c5b4bde1dda52565
Tati_MS <90469788+tatiana-sthl@users.noreply.github.com>
2023-08-05 15:43:05 +0200

    work on stack

--- src/components/about/Bio.jsx
+++ src/components/about/Bio.jsx
@@ -1,10 +1,31 @@
 import React from "react"
+//import stack2023 from "../../images/stack_2023.png"
+import htmlIcon from "../../images/svg/html5-white.svg"
+import cssIcon from "../../images/svg/css3-fill.svg"
+import mongodbIcon from "../../images/svg/mongodb-white.svg"
+
 
 export const Bio = ({ items, Heading }) => {
+
+  const competences = [
+    { nom:"HTML5", icone: htmlIcon },
+    { nom:"CSS3", icone: cssIcon },
+    { nom:"MONGODB", icone: mongodbIcon }
+    
+  ];
+
   return (
     <>
-      <Heading title='Mon stack' />
-      
+      <Heading title="Mon stack" />
+      <div className="competences">
+        {competences.map((competence, index) => (
+          <div key={index} className="competence">
+            <div className="icone-wrapper">
+            <img src={competence.icone} alt={competence.nom} />
+            </div>
+          </div>
+        ))}
+      </div>
     </>
-  )
+  );
 }
