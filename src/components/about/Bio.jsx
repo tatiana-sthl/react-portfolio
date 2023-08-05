51206844d3bb713d75f1ff13db5751a743da7e8a
Tati_MS <90469788+tatiana-sthl@users.noreply.github.com>
2023-08-05 15:59:35 +0200

    add all icons

--- src/components/about/Bio.jsx
+++ src/components/about/Bio.jsx
@@ -2,7 +2,16 @@ import React from "react"
 //import stack2023 from "../../images/stack_2023.png"
 import htmlIcon from "../../images/svg/html5-white.svg"
 import cssIcon from "../../images/svg/css3-fill.svg"
+import javaScriptIcon from "../../images/svg/javascript-fill-white.svg"
+import reactIcon from "../../images/svg/react-fill-white.svg"
+import phpIcon from "../../images/svg/php-fill-white.svg"
 import mongodbIcon from "../../images/svg/mongodb-white.svg"
+import bootstrapIcon from "../../images/svg/bootstrap-fill-white.svg"
+import vscIcon from "../../images/svg/visualstudiocode-white.svg"
+import figmaIcon from "../../images/svg/figma.svg"
+import postmanIcon from "../../images/svg/postman-white.svg"
+import wordpressIcon from "../../images/svg/wordpress-white.svg"
+
 
 
 export const Bio = ({ items, Heading }) => {
@@ -10,8 +19,17 @@ export const Bio = ({ items, Heading }) => {
   const competences = [
     { nom:"HTML5", icone: htmlIcon },
     { nom:"CSS3", icone: cssIcon },
-    { nom:"MONGODB", icone: mongodbIcon }
-    
+    { nom: "JS", icone: javaScriptIcon },
+    { nom: "react", icone: reactIcon },
+    { nom: "php", icone: phpIcon },
+    { nom:"MONGODB", icone: mongodbIcon },
+    { nom:"bootstrap", icone: bootstrapIcon },    
+    { nom:"VSC", icone: vscIcon },    
+    { nom:"figma", icone: figmaIcon },    
+    { nom:"wordpress", icone: wordpressIcon }, 
+    { nom:"postman", icone: postmanIcon },    
+
+
   ];
 
   return (
